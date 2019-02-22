3f1cc770403d1b1ca804f21c580472a76347ef12
c-tel <stas.telizhenko@gmail.com>
2019-02-22 22:40:01 +0200

    refactoring

--- Lab1Telizhenko/Models/WelcomeModel.cs
+++ Lab1Telizhenko/Models/WelcomeModel.cs
@@ -44,38 +44,27 @@ namespace Lab1Telizhenko.Models
         {
             if ((date.Month == 3 && date.Day >= 21) || (date.Month == 4 && date.Day <= 20))
                 return WestZodiac.Aries;
-            else
             if ((date.Month == 4 && date.Day >= 21) || (date.Month == 5 && date.Day <= 20))
                 return WestZodiac.Taurus;
-            else
             if ((date.Month == 5 && date.Day >= 21) || (date.Month == 6 && date.Day <= 21))
                 return WestZodiac.Gemini;
-            else
             if ((date.Month == 6 && date.Day >= 22) || (date.Month == 7 && date.Day <= 22))
                 return WestZodiac.Canser;
-            else
             if ((date.Month == 7 && date.Day >= 23) || (date.Month == 8 && date.Day <= 23))
                 return WestZodiac.Leo;
-            else
             if ((date.Month == 8 && date.Day >= 24) || (date.Month == 9 && date.Day <= 23))
                 return WestZodiac.Virgo;
-            else
             if ((date.Month == 9 && date.Day >= 24) || (date.Month == 10 && date.Day <= 22))
                 return WestZodiac.Libra;
-            else
             if ((date.Month == 10 && date.Day >= 23) || (date.Month == 11 && date.Day <= 22))
                 return WestZodiac.Scorpio;
-            else
             if ((date.Month == 11 && date.Day >= 23) || (date.Month == 12 && date.Day <= 21))
                 return WestZodiac.Sagittarius;
-            else
             if ((date.Month == 12 && date.Day >= 22) || (date.Month == 1 && date.Day <= 20))
                 return WestZodiac.Capricorn;
-            else
             if ((date.Month == 1 && date.Day >= 21) || (date.Month == 2 && date.Day <= 19))
                 return WestZodiac.Aquarius;
-            else
-                return WestZodiac.Pisces;
+            return WestZodiac.Pisces;
         }
 
         private ChineeseZodiac CalculateChineese(DateTime date)
